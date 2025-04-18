051d178b79ef7cfba029f04685e425c7f766620f
Christopher Jacobs <chrisj951@gmail.com>
2025-04-17 18:19:24 -0700

    debug

--- src/gl/loader.c
+++ src/gl/loader.c
@@ -1,6 +1,13 @@
-#include <dlfcn.h>
 #include "loader.h"
 
+
+typedef struct
+{
+  const char *dli_fname;        /* File name of defining object.  */
+  void *dli_fbase;              /* Load address of that object.  */
+  const char *dli_sname;        /* Name of nearest symbol.  */
+  void *dli_saddr;              /* Exact value of nearest symbol.  */
+} Dl_info;
 
 void (APIENTRY_GL4ES *gl4es_getMainFBSize)(GLint* width, GLint* height);
 
